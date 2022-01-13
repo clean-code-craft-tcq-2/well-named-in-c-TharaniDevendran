26cc40b3690f2e297c22d0e1c45f4f42b818c568
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-01-13 18:10:49 +0530

    Update main.c

--- main.c
+++ main.c
@@ -74,12 +74,12 @@ int main() {
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
     printf("Number\t\t\tMajorColor\t\t\tMinorColor");
-    int Count =0;
+    int Count =1;
     for(int i =0; i<numberOfMajorColors;i++)
     {
 	    for(int j=0;j<numberOfMinorColors;j++)
 	    {
-		    printf("%d\n", Count++);
+		    printf("\n%d", Count++);
 		    printf("\t\t\t\%s",MajorColorNames[i]);
 		    printf("\t\t\t%s\n", MinorColorNames[j]);
 	    }
