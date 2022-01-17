1267a8b028afd2d9006c52dcdbea97f6e5393e77
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-01-17 22:04:14 +0530

    Create struct.h

---
+++ struct.h
@@ -0,0 +1,20 @@
+#include <studio.h>
+
+const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet"};
+const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+typedef struct
+{
+	enum MajorColor majorColor;
+	enum MinorColor minorColor;
+} ColorPair;
+
+extern struct Colorpair colorPair;
