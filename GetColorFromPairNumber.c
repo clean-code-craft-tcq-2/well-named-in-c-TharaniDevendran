6d9a494360ff54b5fdbf0351ec00cba415ff4e1c
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-01-17 19:30:42 +0530

    Update GetColorFromPairNumber.c

--- GetColorFromPairNumber.c
+++ GetColorFromPairNumber.c
@@ -2,6 +2,12 @@
 #include <assert.h>
 #include "struct.h"
 
+const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
 ColorPair GetColorFromPairNumber(int pairNumber)
 {
     ColorPair colorPair;
