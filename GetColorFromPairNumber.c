553760294e3cd650414eafba851371b6466bc9ef
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-01-17 18:58:51 +0530

    Create GetColorFromPairNumber.c

---
+++ GetColorFromPairNumber.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <assert.h>
+#include "struct.h"
+
+ColorPair GetColorFromPairNumber(int pairNumber)
+{
+    ColorPair colorPair;
+    int zeroBasedPairNumber = pairNumber - 1;
+    colorPair.majorColor = 
+        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor =
+        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return colorPair;
+}
