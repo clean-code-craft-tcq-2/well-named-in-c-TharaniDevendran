2c1104fcb7eae3482be22d505e17e438d6102dd9
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-01-17 22:09:37 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,25 +2,7 @@
 #include <assert.h>
 #include "testNumberToPair.h"
 #include "PairToNumber.h"
-
-const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet"};
-const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-
-extern int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-extern int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
-const int MAX_COLORPAIR_NAME_CHARS = 16;
-
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
-typedef struct
-{
-	enum MajorColor majorColor;
-	enum MinorColor minorColor;
-} ColorPair;
-
-extern struct Colorpair colorPair;
+#include "struct.h"
 
 int main() {
     testNumberToPair(4, WHITE, BROWN);
