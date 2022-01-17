7abeedcaedb5ae10017951a3590dd9c920fa017c
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-01-17 18:59:41 +0530

    Create GetPairNumberFromColor.c

---
+++ GetPairNumberFromColor.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <assert.h>
+#include "struct.h"
+
+int GetPairNumberFromColor(const ColorPair* colorPair) {
+    return colorPair->majorColor * numberOfMinorColors +
+            colorPair->minorColor + 1;
+}
