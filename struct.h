c0637eb3372c5bd409b42c4a6063552056eec714
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-01-17 19:01:01 +0530

    Update struct.h

--- struct.h
+++ struct.h
@@ -1,5 +1,8 @@
 #define STRUCT_H  
 
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
