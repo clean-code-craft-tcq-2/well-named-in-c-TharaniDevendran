978d6ec118ad795553a5275b47e60dfad75f5b45
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-01-17 18:57:38 +0530

    Create ColorPairToString.c

---
+++ ColorPairToString.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <assert.h>
+#include "struct.h"
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer) {
+    sprintf(buffer, "%s %s",
+        MajorColorNames[colorPair->majorColor],
+        MinorColorNames[colorPair->minorColor]);
+}
