d138585342d9f853e0e113fabe313f88e4b3f698
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-01-18 21:32:07 +0530

    Update struct.h

--- struct.h
+++ struct.h
@@ -22,3 +22,5 @@ extern struct Colorpair colorPair;
 ColorPair GetColorFromPairNumber(int pairNumber);
 int GetPairNumberFromColor(const ColorPair* colorPair);
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
+void testNumberToPair(int pairNumber,enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber);
