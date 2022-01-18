b201b9f0b0643f6a762882678c0e2f3facfe11c5
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-01-18 19:21:33 +0530

    Update struct.h

--- struct.h
+++ struct.h
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
-const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet"};
-const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+extern const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet"};
+extern const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
-int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+extern int numberOfMajorColors ;
+numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+extern int numberOfMinorColors;
+numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0])
 
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+extern enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+extern enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
 typedef struct
 {
