4e20047ca109a4ee6264c9072d1b156ce61c499e
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-01-18 22:04:35 +0530

    Update struct.h

--- struct.h
+++ struct.h
@@ -22,3 +22,4 @@ int GetPairNumberFromColor(const ColorPair* colorPair);
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
 void testNumberToPair(int pairNumber,enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber);
+void DisplayColorCodeManual();
